89791b10fe9e2aca8d9b2a8be6073343fc1e515f
RATHINAMOORTHY <nrathinamoorthy@gmail.com>
2019-06-02 11:23:39 +0530

    first commit

---
+++ MyProject/MyApplication/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def MyApplicationView(request):
+    return HttpResponse("<h1>Welcome to Mac Family</h1>")
